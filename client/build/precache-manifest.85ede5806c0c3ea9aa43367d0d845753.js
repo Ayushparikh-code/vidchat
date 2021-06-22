e930b44e770d2bbb927b18479bba7387368a3211
Ayush Parikh <ayushparikh332@gmail.com>
2021-06-22 11:36:24 +0530

    adding files & folders

---
+++ client/build/precache-manifest.85ede5806c0c3ea9aa43367d0d845753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439fe550d650b8b170ab387d388b2ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99ed958a73e4f6f17156",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "2af5c5b165584ee9bb4f",
+    "url": "/static/js/2.11527d2e.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.11527d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ed958a73e4f6f17156",
+    "url": "/static/js/main.87714d16.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './components/Main/Main';
+import Room from './components/Room/Room'
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContainer>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/room/:roomId" component={Room} />
+        </Switch>
+      </AppContainer>
+    </BrowserRouter>
+  );
+}
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(8px + 2vmin);
+  color: white;
+  background: #ff9966;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #ff5e62, #ff9966);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #ff5e62, #ff9966); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  text-align: center;
+`;
+
+export default App;
